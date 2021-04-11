3d8bc19083f7225c3e5d8e09f85643251476d803
BADBADBADBOY <15671628547@163.COM>
2021-04-11 16:42:46 +0800

    fix bug

--- models/backbone/resnet.py
+++ models/backbone/resnet.py
@@ -203,10 +203,10 @@ class ResNet(nn.Module):
             block, 256, layers[2], stride=2, dcn=dcn)
         self.layer4 = self._make_layer(
             block, 512, layers[3], stride=2, dcn=dcn)
-        self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        #self.avgpool = nn.AvgPool2d(7, stride=1)
+        #self.fc = nn.Linear(512 * block.expansion, num_classes)
 
-        self.smooth = nn.Conv2d(2048, 256, kernel_size=1, stride=1, padding=1)
+        #self.smooth = nn.Conv2d(2048, 256, kernel_size=1, stride=1, padding=1)
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
